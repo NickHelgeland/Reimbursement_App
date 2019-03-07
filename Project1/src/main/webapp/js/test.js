ea95a4b00eace89484cba055de17118863551894
Nick Helgeland <nickhelgeland94@gmail.com>
2019-03-07 09:55:52 -0500

    Retrieved data from a json and displayed it on our html page.

--- Project1/src/main/webapp/js/test.js
+++ Project1/src/main/webapp/js/test.js
@@ -2,27 +2,35 @@ $(function() {
 	testGet();
 });
 
-function get(url) {	
-	$.get(url, function(data, status) {
+function get(url) 
+{	
+	$.get(url, function(data, status) 
+	{
+		console.log(data);
+		document.getElementById("header").innerHTML = data[0].firstName;
+	});
+}
+
+function post(url, data) 
+{	
+	$.post(url, JSON.stringify(data), function(data, status) 
+	{
 		console.log(data);
 	});
 }
 
-function post(url, data) {	
-	$.post(url, JSON.stringify(data), function(data, status) {
-		console.log(data);
-	});
-}
-
-function testGet() {
+function testGet() 
+{
 	url = '/TestServlet';
 	
 	get(url);
 }
 
-function testPost() {
+function testPost() 
+{
 	url = '/TestServlet';
-	data = {
+	data = 
+	{
 		"key1": "hello world"
 	};
 	
