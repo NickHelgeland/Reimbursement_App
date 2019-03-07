51c52c9c2feda28628d6b2ddd1e3ccbcc028f8d6
Nick Helgeland <nickhelgeland94@gmail.com>
2019-03-06 21:50:42 -0500

    Set up end to end connection

--- Project1/src/main/java/com/revature/servlet/TestServlet.java
+++ Project1/src/main/java/com/revature/servlet/TestServlet.java
@@ -1,6 +1,8 @@
 package com.revature.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.database.EmployeeDAO;
+import com.revature.model.Employee;
 
 /**
  * Servlet implementation class TestServlet
@@ -30,7 +34,27 @@ public class TestServlet extends HttpServlet
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
 	{
-		System.out.println("doGet");
+		EmployeeDAO dao = new EmployeeDAO();
+		
+		Employee employee = new Employee();
+		employee.setEmployeeId(1);
+		Employee fullEmployee = null;
+		try 
+		{
+			fullEmployee = dao.selectOne(employee);
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		ObjectMapper mapper = new ObjectMapper();
+		PrintWriter out = response.getWriter();
+		String employeeJSON;
+		employeeJSON = mapper.writeValueAsString(fullEmployee);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		out.print(employeeJSON);
+		out.flush();
 	}
 
 	/**
--- Project1/src/main/webapp/js/test.js
+++ Project1/src/main/webapp/js/test.js
@@ -1,5 +1,5 @@
 $(function() {
-	testPost();
+	testGet();
 });
 
 function get(url) {	
