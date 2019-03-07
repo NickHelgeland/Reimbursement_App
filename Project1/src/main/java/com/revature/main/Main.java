76c7af214acf762fb381576633d0e98201beb814
Nick Helgeland <nickhelgeland94@gmail.com>
2019-03-06 21:03:26 -0500

    Added some database connection classes

--- Project1/src/main/java/com/revature/main/Main.java
+++ Project1/src/main/java/com/revature/main/Main.java
@@ -1,5 +1,33 @@
 package com.revature.main;
 
-public class Main {
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import com.revature.database.EmployeeDAO;
+import com.revature.model.Employee;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		EmployeeDAO dao = new EmployeeDAO();
+		
+		Employee employee = new Employee();
+		
+		employee.setEmployeeId(1);
+		
+		try 
+		{
+			ArrayList<Employee> list = dao.selectAll();
+			
+			for(Employee e : list)
+			{
+				System.out.println(e.getFirstName() + e.getLastName());
+			}
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 }
